8a35ce19ee1b650c5ec650a01f108d55489ebec8
liang <343093497@qq.com>
2019-09-03 17:11:48 +0800

    7

---
+++ countwords.py
@@ -0,0 +1,3 @@
+#!/usr/bin/env python3
+s = input('shuru:')
+print('shuchudejieguo %d' % (len(s.split(' '))))
---
+++ palindrome.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+s = input('shuru:')
+z= s[::-1]
+if s == z:
+	print(':)')
+else:
+	print(':(')
