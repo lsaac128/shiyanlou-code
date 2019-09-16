1df1794780dbbe7a99272ac6cd98a38a0765d931
wangliang <343093497@qq.com>
2019-09-17 00:01:22 +0800

    wanshan_1

--- calculator_1.py
+++ calculator_1.py
@@ -9,25 +9,19 @@ for x,y in dict_1.items():
 	yjssde = int(y) - 0.165*int(y) - 5000
 	if yjssde <= 0:
 		gongzi = format(int(y) - 0.165*int(y), '.2f')
-		print('{}:{}'.format(x,gongzi))
 	elif yjssde > 0 and yjssde <= 3000:
 		gongzi = format(int(y) - 0.165*int(y) - yjssde * 0.03, '.2f')
-		print('{}:{}'.format(x,gongzi))
 	elif yjssde > 3000 and yjssde <= 12000:
 		gongzi = format(int(y) - 0.165*int(y) - (yjssde * 0.1 - 210), '.2f')
-		print('{}:{}'.format(x,gongzi))
 	elif yjssde > 12000 and yjssde <= 25000:
 		gongzi = format(int(y) - 0.165*int(y) - (yjssde * 0.2 - 1410), '.2f')
-		print('{}:{}'.format(x,gongzi))
 	elif yjssde > 25000 and yjssde <= 35000:
 		gongzi = format(int(y) - 0.165*int(y) - (yjssde * 0.25 - 2660), '.2f')
-		print('{}:{}'.format(x,gongzi))
 	elif yjssde > 35000 and yjssde <= 55000:
 		gongzi = format(int(y) - 0.165*int(y) - (yjssde * 0.3 - 4410), '.2f')
-		print('{}:{}'.format(x,gongzi))
 	elif yjssde > 55000 and yjssde <= 80000:
 		gongzi = format(int(y) - 0.165*int(y) - (yjssde * 0.35 - 7160), '.2f')
-		print('{}:{}'.format(x,gongzi))
+	
 	elif yjssde > 80000:
 		gongzi = format(int(y) - 0.165*int(y) - (yjssde * 0.45 - 15160), '.2f')
-		print('{}:{}'.format(x,gongzi))
+	print('{}:{}'.format(x,gongzi))
