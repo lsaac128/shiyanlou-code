60de9c0eab98b04b900c68d70b3a8f5f1f0f0c43
wang <343093497@qq.com>
2019-10-02 12:33:24 +0800

    挑战，一个简单的资讯网站

--- news/app.py
+++ news/app.py
@@ -9,16 +9,19 @@ app.config['TEMPLATE_AUTO_RELOAD'] = True
 
 @app.route('/')
 def index():
+    title_list = []
     files_list = os.listdir('/home/shiyanlou/files/')
     for i in files_list:
         lujin = '/home/shiyanlou/files/' + i
         with open(lujin) as f:
-            title_list = []
             a = f.read()
             b = json.loads(a)
             title_list.append(b['title'])
-    return title_list
+    return render_template('index.html', title_list=title_list)
 
 @app.route('/files/<filename>')
 def file(filename):
-    pass
+    content_list = []
+    files_contebt = os.listdir('/home/shiyanlou/files')
+    lujin2 = '/home/shiyanlou/files/' + filename
+    return lujin2
